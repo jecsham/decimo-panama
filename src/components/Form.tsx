6ac94a2e786fba77ed5e34a9c3db08be91e1e235
jecsham <jecsham27@gmail.com>
2019-11-30 00:05:35 -0500

    added dynamic SS

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,7 +7,9 @@ class Form extends React.Component<any, any> {
             month1: '',
             month2: '',
             month3: '',
-            month4: ''
+            month4: '',
+            segurosocial: '9.75',
+            seguroeducativo: '1.25',
         }
         this.handleChange = this.handleChange.bind(this);
     }
@@ -21,7 +23,7 @@ class Form extends React.Component<any, any> {
     }
 
     calculate = () => {
-        let { month1, month2, month3, month4 } = this.state;
+        let { month1, month2, month3, month4, segurosocial } = this.state;
         if (month1 === '' || month2 === '' || month3 === '' || month4 === '') {
             month1 = 0;
             month2 = 0;
@@ -31,7 +33,7 @@ class Form extends React.Component<any, any> {
 
         let total = parseFloat(month1) + parseFloat(month2) + parseFloat(month3) + parseFloat(month4);
         let decimo: any = (total / 12).toFixed(2);
-        let ssocial: any = ((7.25 / 100) * decimo).toFixed(2);
+        let ssocial: any = ((parseFloat(segurosocial) / 100) * decimo).toFixed(2);
         let decimoFinal = (decimo - ssocial).toFixed(2);
         this.setState({ total, decimo, ssocial, decimoFinal, calculated: true }, () => {
             this.senData();
@@ -40,27 +42,39 @@ class Form extends React.Component<any, any> {
 
     render() {
         return (
-            <div>
-                <form className="Form">
-                    <div className="uk-margin-top">
-                        <label htmlFor="month1" className="uk-margin-small-right uk-text-secondary" >Mes 1</label>
-                        <input id="month1" type="number" value={this.state.month1} onChange={this.handleChange} className="uk-box-shadow-hover-small uk-input uk-form-width-medium uk-form-small" />
+            <div className="uk-flex uk-flex-center">
+                <div className="uk-width-medium">
+                    <div className="uk-width-medium">
+                        <div className="uk-margin-top">
+                            <label htmlFor="month1" className="uk-margin-small-right uk-text-secondary" >Mes 1</label>
+                            <input id="month1" type="number" value={this.state.month1} onChange={this.handleChange} className="uk-box-shadow-hover-small uk-input uk-form-width-medium uk-form-small" />
+                        </div>
+                        <div className="uk-margin-top">
+                            <label htmlFor="month2" className="uk-margin-small-right uk-text-secondary" >Mes 2</label>
+                            <input id="month2" type="number" value={this.state.month2} onChange={this.handleChange} className="uk-box-shadow-hover-small uk-input uk-form-width-medium uk-form-small" />
+                        </div>
+                        <div className="uk-margin-top">
+                            <label htmlFor="month3" className="uk-margin-small-right uk-text-secondary" >Mes 3</label>
+                            <input id="month3" type="number" value={this.state.month3} onChange={this.handleChange} className="uk-box-shadow-hover-small uk-input uk-form-width-medium uk-form-small" />
+                        </div>
+                        <div className="uk-margin-top">
+                            <label htmlFor="month4" className="uk-margin-small-right uk-text-secondary" >Mes 4</label>
+                            <input id="month4" type="number" value={this.state.month4} onChange={this.handleChange} className="uk-box-shadow-hover-small uk-input uk-form-width-medium uk-form-small" />
+                        </div>
+                            <div className="uk-margin-top">
+                                <label htmlFor="segurosocial" className="uk-margin-small-right uk-text-secondary">% Seguro Social</label>
+                                <input id="segurosocial" type="number" value={this.state.segurosocial} onChange={this.handleChange} className="uk-form-controls uk-box-shadow-hover-small uk-input uk-form-width-small uk-form-small" />
+                            </div>
+                        <div className="uk-grid uk-margin-top">
+                            {/* <div className="uk-width-expand uk-form-stacked">
+                                <label htmlFor="seguroeducativo" className="uk-form-label uk-text-muted uk-text-small">Seguro Educativo</label>
+                                <input id="seguroeducativo" type="number" value={this.state.seguroeducativo} onChange={this.handleChange} className="uk-form-controls uk-box-shadow-hover-small uk-input uk-form-width-small uk-form-small" />
+                            </div> */}
+                        </div>
                     </div>
                     <div className="uk-margin-top">
-                        <label htmlFor="month2" className="uk-margin-small-right uk-text-secondary" >Mes 2</label>
-                        <input id="month2" type="number" value={this.state.month2} onChange={this.handleChange} className="uk-box-shadow-hover-small uk-input uk-form-width-medium uk-form-small" />
+                        <button className="uk-box-shadow-hover-small uk-button uk-button-default uk-button-small" onClick={this.calculate}>Calcular</button>
                     </div>
-                    <div className="uk-margin-top">
-                        <label htmlFor="month3" className="uk-margin-small-right uk-text-secondary" >Mes 3</label>
-                        <input id="month3" type="number" value={this.state.month3} onChange={this.handleChange} className="uk-box-shadow-hover-small uk-input uk-form-width-medium uk-form-small" />
-                    </div>
-                    <div className="uk-margin-top">
-                        <label htmlFor="month4" className="uk-margin-small-right uk-text-secondary" >Mes 4</label>
-                        <input id="month4" type="number" value={this.state.month4} onChange={this.handleChange} className="uk-box-shadow-hover-small uk-input uk-form-width-medium uk-form-small" />
-                    </div>
-                </form>
-                <div className="uk-margin-top">
-                    <button className="uk-box-shadow-hover-small uk-button uk-button-default uk-button-small" onClick={this.calculate}>Calcular</button>
                 </div>
             </div>
         )
